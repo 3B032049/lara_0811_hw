1cec8cc8531260ea2f1618a59eecc76a61a60bca
鄭哲丞 <a0968518713@gmail.com>
2023-11-13 23:16:02 +0800

    新增route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +15,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+
+Route::get('home', [HomeController::class, 'index'])->name('home.index');;
+
+Route::get('about', [AboutController::class, 'index'])->name('about.index');;
+
+Route::get('news', [NewsController::class, 'index'])->name('news.index');;
